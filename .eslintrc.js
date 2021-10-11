b40ff41da5c329f88d000484f04639b47ebf4f9d
Princesseuh <princssdev@gmail.com>
2021-10-11 01:21:51 -0400

    Fix Typescript errors being duplicated

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier"],
   extends: [
-    "plugin:@typescript-eslint/recommended",
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
   overrides: [

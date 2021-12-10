21b4b3ada67669b5338a5874857579419d25a24c
Princesseuh <princssdev@gmail.com>
2021-12-10 20:47:57 +0100

    Update website address

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,6 +9,6 @@ export function getSlugFromFile(path: string): string {
 export function getBaseSiteURL(): string {
   // NOTE: This produces an error but it works, who knows
   return import.meta.env.PROD
-    ? "https://hungry-newton-542b56.netlify.app/"
+    ? "https://princesseuh.netlify.app/"
     : "http://localhost:3000"
 }

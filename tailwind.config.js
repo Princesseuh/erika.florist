8279adc2c04891348b50a73aeff3a7d0be937d68
Princesseuh <princssdev@gmail.com>
2021-12-10 20:25:30 +0100

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const plugin = require("tailwindcss/plugin")
 
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{astro,js,ts,tsx,md}", "safelist.txt"],
+  content: ["./src/**/*.{astro,js,ts,tsx,md}", "safelist.txt"],
   corePlugins: {
     preflight: false,
 

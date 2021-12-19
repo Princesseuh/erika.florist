1a6de08ec1d7e50c5352f5fcde2cefbd88fb8596
Princesseuh <princssdev@gmail.com>
2021-12-19 23:07:02 +0100

    Fix getting dates from Git

--- src/data/wiki.ts
+++ src/data/wiki.ts
@@ -23,17 +23,11 @@ function postProcessWikiItem(wikiItem: WikiItem): WikiItem {
   if (import.meta.env.PROD) {
     // Get the last modified time from Git as getting it from file system is not accurate
     // PERF: This can be a bit slow, in Eleventy it used to slow down my builds a lot
-    try {
-      const isoDate = execSync(
-        `git log -1 --date=iso --pretty="format:%cI" -- ./${wikiItem.file.pathname}`,
-      )
+    const isoDate = execSync(
+      `git log -1 --date=iso --pretty="format:%cI" -- ./${wikiItem.file.pathname}`,
+    )
 
-      wikiItem.lastModified = new Date(Date.parse(isoDate.toString()))
-    } catch {
-      // In most cases, if we get an error here it's because the file doesn't exist yet in Git's files
-      // So whenever we get an error, we'll just create a date from current date (which is accurate)
-      wikiItem.lastModified = new Date()
-    }
+    wikiItem.lastModified = new Date(Date.parse(isoDate.toString()))
   } else {
     wikiItem.lastModified = new Date()
   }

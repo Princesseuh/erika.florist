68fffc9d0560b9d0902c4e0e3b9094821259434d
Princesseuh <princssdev@gmail.com>
2022-09-16 16:39:03 -0300

    Use proper output

--- apps/cms/astro.config.mjs
+++ apps/cms/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   adapter: netlify(),
   integrations: [tailwind()],
   server: {

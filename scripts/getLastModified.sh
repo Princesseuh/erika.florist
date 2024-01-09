4476d30923bfc43b3542476bcbc8a447399665a2
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-01-08 22:29:33 -0500

    fix: move unshallow script before everything

--- scripts/getLastModified.sh
+++ scripts/getLastModified.sh
@@ -1,10 +1,3 @@
-# Unshallow the repository when running on Vercel
-# Vercel doesn't register the remote in the local repository so the full URL is used.
-# Also, the branch is always named master on Vercel's build system
-if [[ $VERCEL == "1" ]]; then
-  git pull --unshallow https://github.com/Princesseuh/erika.florist.git "${PUBLIC_VERCEL_GIT_COMMIT_SHA}:master" >/dev/null 2>&1
-fi
-
 find src/content/wiki -name "*.mdx"|while read fname; do
   echo "$fname|"
   git log -1 --date=iso --pretty="format:%cI|%H;" -- "$fname"

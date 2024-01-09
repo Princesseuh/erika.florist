4f1dd120b476970bddd3fc8f33ac9b59acec4965
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-01-08 22:03:00 -0500

    chore: lockfile

--- scripts/getLastModified.sh
+++ scripts/getLastModified.sh
@@ -2,7 +2,7 @@
 # Vercel doesn't register the remote in the local repository so the full URL is used.
 # Also, the branch is always named master on Vercel's build system
 if [[ $VERCEL == "1" ]]; then
-  git pull --unshallow https://github.com/Princesseuh/erika.florist.git "${PUBLIC_VERCEL_GIT_COMMIT_SHA}:master"
+  git pull --unshallow https://github.com/Princesseuh/erika.florist.git "${PUBLIC_VERCEL_GIT_COMMIT_SHA}:master" >/dev/null 2>&1
 fi
 
 find src/content/wiki -name "*.mdx"|while read fname; do

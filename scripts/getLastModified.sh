ff78824d8dd28e9cd4f49884f510684fe2b9b70e
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-01-08 21:58:26 -0500

    fix: bash syntax

--- scripts/getLastModified.sh
+++ scripts/getLastModified.sh
@@ -1,7 +1,7 @@
 # Unshallow the repository when running on Vercel
 # Vercel doesn't register the remote in the local repository so the full URL is used.
 # Also, the branch is always named master on Vercel's build system
-if [ $VERCEL == "1" ]; do
+if [[ $VERCEL == "1" ]]; then
   git pull --unshallow https://github.com/Princesseuh/erika.florist.git "${PUBLIC_VERCEL_GIT_COMMIT_SHA}:master"
 fi
 

07b957faac9cc503dfebf9be7d3c29c3be770503
Douglas Pimentel <dev.pimentel@gmail.com>
2024-02-17 20:50:43 -0300

    fix

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,23 +1,23 @@
 import type { APIContext } from "astro";
 import { getCollection } from "astro:content";
-import rss, { type RSSOptions, } from "@astrojs/rss";
+import rss, { type RSSOptions, type RSSFeedItem } from "@astrojs/rss";
 
 export async function GET(context: APIContext) {
-  const blog = await getCollection("blog");
+	const blog = await getCollection("blog");
 
-  const allPosts = blog.map((post) => ({
-    title: post.data.title,
-    pubDate: post.data.date,
-    description: post.data.tagline,
-    link: `/articles/${post.slug}/`,
-  }));
+	const items: RSSFeedItem[] = blog.map((post) => ({
+		title: post.data.title,
+		pubDate: post.data.date,
+		description: post.data.tagline,
+		link: `/articles/${post.slug}/`,
+	}));
 
-  const opts: RSSOptions = {
-    title: "The Erika Blog",
-    description: "Articles written by Erika.",
-    site: context.site,
-    items: allPosts,
-  };
+	const opts: RSSOptions = {
+		title: "The Erika Blog",
+		description: "Articles written by Erika.",
+		site: context.site,
+		items,
+	};
 
-  return rss(opts);
+	return rss(opts);
 }

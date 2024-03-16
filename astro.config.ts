40d4d18e87a10e717b2b53f2cb00de25e76ee303
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-03-16 13:09:19 +0100

    feat: trying a funky way of doing RSS

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import expressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
 
 import markdoc from "@astrojs/markdoc";
+import { rename } from "fs/promises";
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,5 +25,20 @@ export default defineConfig({
 		markdoc({
 			allowHTML: true,
 		}),
+		{
+			name: "RSS Generator",
+			hooks: {
+				"astro:build:done": async (options) => {
+					const rssPages = options.pages.filter((page) => page.pathname.includes("rss"));
+
+					for (const { pathname: rssPage } of rssPages) {
+						await rename(
+							new URL(`${rssPage}index.html`, options.dir),
+							new URL(`${rssPage}index.xml`, options.dir),
+						);
+					}
+				},
+			},
+		},
 	],
 });

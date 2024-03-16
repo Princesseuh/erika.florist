244a4636b1fd99e96d2d5975062873cf87ca4d34
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-03-16 20:39:44 +0100

    fix: cleaning up RSS feed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { getBaseSiteURL } from "$utils";
 import { decode } from "tiny-decode";
-import { ELEMENT_NODE, transform, walk } from "ultrahtml";
+import { ELEMENT_NODE, TEXT_NODE, transform, walk, type TextNode } from "ultrahtml";
 import sanitize from "ultrahtml/transformers/sanitize";
 import { defineMiddleware } from "astro:middleware";
 
@@ -13,6 +13,7 @@ export const onRequest = defineMiddleware(async (ctx, next) => {
 		const output = await transform(html, [
 			async (node) => {
 				await walk(node, (node) => {
+					// Simplify picture elements to img elements, some feeds are struggling with it
 					if (node.type === ELEMENT_NODE && node.name === "picture") {
 						if (node.parent.type === ELEMENT_NODE && node.parent.name === "a") {
 							const imgChildren = node.children.find(
@@ -31,6 +32,32 @@ export const onRequest = defineMiddleware(async (ctx, next) => {
 							// biome-ignore lint/performance/noDelete: <explanation>
 							delete node.attributes.style;
 						}
+					}
+
+					// Make sure links are absolute, some readers are not smart enough to figure it out
+					if (
+						node.type === ELEMENT_NODE &&
+						node.name === "a" &&
+						node.attributes.src?.startsWith("/")
+					) {
+						node.attributes.src = getBaseSiteURL().slice(0, -1) + node.attributes.src;
+					}
+
+					// Remove favicon images, some readers don't know they should be inline and it ends up being a broken image
+					if (
+						node.type === ELEMENT_NODE &&
+						("data-favicon" in node.attributes || "data-favicon-span" in node.attributes)
+					) {
+						node = node as unknown as TextNode;
+						node.type = TEXT_NODE;
+						node.value = "";
+					}
+
+					// Remove EC buttons
+					if (node.type === ELEMENT_NODE && node.attributes["data-code"]) {
+						node = node as unknown as TextNode;
+						node.type = TEXT_NODE;
+						node.value = "";
 					}
 				});
 

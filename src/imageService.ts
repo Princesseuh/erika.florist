089d9dfb29c2ddb3c2195ac26a449103d2a0c1ce
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2025-08-07 00:58:19 +0200

    feat: license

--- src/imageService.ts
+++ src/imageService.ts
@@ -6,6 +6,22 @@ import sharp from "sharp";
 
 const CACHE_PATH = "./node_modules/.astro/placeholders/";
 
+// Adapted from https://github.com/ampproject/amp-toolbox/blob/d9a213b7a8245f176c9508fdf7dcd1255670aa4e/packages/optimizer/lib/transformers/AddBlurryImagePlaceholders.js#L268
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 function getBitmapDimensions(
 	imgWidth: number,
 	imgHeight: number,
